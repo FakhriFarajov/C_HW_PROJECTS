a87839023ab488f0969eeae2d3d3fe082c2ae063
FakhriFarajov <farajooaze@gmail.com>
2025-04-09 14:36:47 +0400

    Leccon5

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Channels;
+
+List<int> integerList = new List<int>()
+{
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
+};
+
+
+async Task<List<int>> FilterEvenAsync(List<int> numbers)
+{
+    await Task.Delay(1500);
+    numbers = numbers.Where(x => x % 2 == 0).ToList();
+    return numbers;
+}
+
+
+async Task<List<int>> FilterOddAsync(List<int> numbers)
+{
+    await Task.Delay(1500);
+    numbers = numbers.Where(x => x % 2 == 1).ToList();
+    return numbers;
+}
+
+
+async Task<int> CalculateSumAsync(List<int> numbers)
+{
+    int sum = numbers.Sum();
+    await Task.Delay(1000);
+    return sum;
+}
+
+
+
+
+#region Task1
+integerList.ToList().ForEach(i => Console.WriteLine(i));
+#endregion
+
+#region Task2
+var resultEven = await FilterEvenAsync(integerList);
+resultEven.ForEach(i => Console.WriteLine(i));
+#endregion
+
+
+#region Task3
+var resultOdd = await FilterOddAsync(integerList);
+resultOdd.ForEach(i => Console.WriteLine(i));
+#endregion
+
+
+#region Task4
+var resultSum = await CalculateSumAsync(integerList);
+Console.WriteLine(resultSum);
+#endregion
---
+++ WpfApp5/WpfApp5/service/FileName.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cars.Service
+{
+    internal class CarsService
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+        public async Task<string> GetVehicleDataAsync(string make, string model)
+        {
+            var requestUrl = $"https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/all-vehicles-model/records?limit=20&refine=model%3A%22{model}%22&refine=make%3A%22{make}%22";
+            var response = await _httpClient.GetAsync(requestUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+    
+            throw new Exception("Failed to get vehicle data");
+        }
+
+
+
+    }
+}
