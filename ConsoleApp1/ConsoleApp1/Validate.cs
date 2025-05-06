c352ff703455a2e603b8af43fc0944455c27514a
FakhriFarajov <143260296+FakhriFarajov@users.noreply.github.com>
2025-05-06 23:49:44 +0400

    Update Validate.cs

--- ConsoleApp1/ConsoleApp1/Validate.cs
+++ ConsoleApp1/ConsoleApp1/Validate.cs
@@ -8,7 +8,8 @@ public static class ValidateService
 {
     private static Regex loginRegex = new Regex(@"^(?=.*[A-Za-z0-9]$)[A-Za-z]([A-Za-z\d.-_]){0,19}$");
     private static Regex passwordRegex = new Regex(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%&_])[A-Za-z\d!@#$%&_]{8,16}$");
-    
+        private static Regex emailRegex = new Regex(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+
     public static bool ValidateLogin(string Login, string Password)
     {
         return loginRegex.IsMatch(Login) && passwordRegex.IsMatch(Password);
@@ -21,6 +22,6 @@ public static class ValidateService
     
     public static bool ValidateEmail(string Email)
     {
-        return loginRegex.IsMatch(Email);
+        return emailRegex.IsMatch(Email);
     }
 }
