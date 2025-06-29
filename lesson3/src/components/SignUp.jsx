3adc73b5000f4e50d2d87d3931ed64a2815ecad3
FakhriFarajov <farajooaze@gmail.com>
2025-06-29 16:20:05 +0400

    New

--- lesson3/src/components/SignUp.jsx
+++ lesson3/src/components/SignUp.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-
 export default class SignUp extends React.Component {
-    return (){
-        <div className="SignForm">
+    render() {
+        return (
+            <div className="SignForm">
                 <h1>Sign Up</h1>
                 <input type="text" placeholder="Username" />
                 <input type="password" placeholder="Password" />
                 <button>Sign Up</button>
-        </div>
+            </div>
+        );
     }
 }
